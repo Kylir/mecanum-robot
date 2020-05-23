6190a96f9479f9e653dc6b51c21c2dc0e2e4ed81
Kylir <jason.forest@glyphe.org>
2020-05-23 22:43:57 +0100

    display only analog debug

--- src/controllerUtils.js
+++ src/controllerUtils.js
@@ -12,18 +12,18 @@ if (list.length < 1) {
 var device = list[0]
 
 //Open device, return gamepad object:
-var gamepad = ds.open(device)
-
-// , {
-//     smoothAnalog: 10,
-//     smoothMotion: 15,
-//     joyDeadband: 4,
-//     moveDeadband: 4,
-// })
+var gamepad = ds.open(device, {
+    smoothAnalog: 10,
+    smoothMotion: 15,
+    joyDeadband: 4,
+    moveDeadband: 4,
+})
 
 gamepad.onmotion = true
 gamepad.onstatus = true
 
 gamepad.onupdate = function (changed) {
-    console.log(changed)
+    if (changed.analog) {
+        console.log(changed)
+    }
 }
