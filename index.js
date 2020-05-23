92a214b40fe080c79e35853ae697a28b3766635e
Kylir <jason.forest@glyphe.org>
2020-05-23 23:14:23 +0100

    controllerUtils as a module

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const gpioUtils = require('./src/gpioUtils')
+const { initController } = require('./src/controllerUtils')
 
 /*
 gpioUtils.moveLeft(50);
@@ -7,5 +8,10 @@ setTimeout(gpioUtils.moveRight, 4000, 50);
 setTimeout(gpioUtils.moveRight, 6000, -50);
 setTimeout(gpioUtils.stop, 8000);
 */
-gpioUtils.allMotorsForward(30)
-setTimeout(gpioUtils.allMotorsStop, 2000)
+let controller = initController()
+controller.onanalog = function (axis, value) {
+    console.log(axis, value)
+}
+
+//gpioUtils.allMotorsForward(30)
+//setTimeout(gpioUtils.allMotorsStop, 2000)
--- src/controllerUtils.js
+++ src/controllerUtils.js
@@ -1,24 +1,23 @@
 const ds = require('dualshock')
 
-var list = ds.getDevices()
+function initController() {
+    const list = ds.getDevices()
 
-console.log('Devices:', list)
-if (list.length < 1) {
-    console.log('Could not find a controller!')
-    process.exit()
+    console.log('Devices:', list)
+    if (list.length < 1) {
+        console.log('Could not find a controller!')
+        return null
+    }
+
+    //Open device, return gamepad object:
+    let controller = ds.open(list[0], {
+        smoothAnalog: 10,
+        smoothMotion: 15,
+        joyDeadband: 4,
+        moveDeadband: 4,
+    })
+
+    return controller
 }
 
-//Get gamepad's device object:
-var device = list[0]
-
-//Open device, return gamepad object:
-var gamepad = ds.open(device, {
-    smoothAnalog: 10,
-    smoothMotion: 15,
-    joyDeadband: 4,
-    moveDeadband: 4,
-})
-
-gamepad.onanalog = function (axis, value) {
-    console.log(axis, value)
-}
+module.exports = { initController }
